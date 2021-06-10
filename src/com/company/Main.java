5c66fa55dda39d7118266962069e1e1c02a25674
skillsapphire <skillsapphire@gmail.com>
2021-06-10 17:27:11 +0200

    implemented review comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int z = 55+6;
+        Integer z = 55+6;
         System.out.println(z);
     }
 }
--- src/com/company/dev2/MyDev2.java
+++ src/com/company/dev2/MyDev2.java
@@ -1,6 +1,5 @@
 public class MyDev2{
     public void test(){
-        System.out.println("Test method");
         Integer x = 5 +6;
         System.out.println(x);
     }
