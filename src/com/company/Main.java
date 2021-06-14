6f56bcf8c39f5bea0e390cbb1566b6b4fefccbf6
skillsapphire <skillsapphire@gmail.com>
2021-06-14 11:32:05 +0200

    removed comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Integer z = 55+6;
         System.out.println(z);
         System.out.println("Main method");
